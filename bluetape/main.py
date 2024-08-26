0141e35886f36b1137d940dd21b16e55839f6f92
ANDRE_PARREIRAS <parreirasandre950@gmail.com>
2024-08-26 18:17:24 -0300

    sucesso em salvar os dados da primeira empresa

--- bluetape/main.py
+++ bluetape/main.py
@@ -7,6 +7,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import requests
+from lxml import html
 
 servico = Service(ChromeDriverManager().install())
 
@@ -14,6 +16,7 @@ navegador = webdriver.Chrome(service=servico)
 
 # Acessar a página inicial do Reclame Aqui
 navegador.get("https://www.reclameaqui.com.br/")
+
 
 # Encontrar o botão "Moda" (ajuste o seletor CSS conforme a estrutura da página)
 botao_moda = navegador.find_element(By.XPATH, "/html/body/section[2]/div/astro-island/div/nav/div[2]/button[4]")
@@ -25,35 +28,38 @@ dados = []
 
 try:
     botao_melhor1 = WebDriverWait(navegador, 10).until(
-        EC.presence_of_element_located((By.XPATH, "/html/body/section[2]/div/astro-island/div/div[3]/div/div[1]/a[1]/span"))
-    )
+        EC.presence_of_element_located((By.XPATH, "/html/body/section[2]/div/astro-island/div/div[3]/div/div[1]/a[1]/span")))
     botao_melhor1.click()
 except:
-    print("Botão 'Moda' não encontrado")
+    print("Botão não encontrado")
+    
+html_content = navegador.page_source
+soup = BeautifulSoup(html_content, 'html.parser')
 
-try:
 
-    html_content = navegador.page_source
-    soup = BeautifulSoup(html_content, 'html.parser')
-    reclamacoes_respondidas = soup.find("span", class_="go2549335548")
-    voltariam_a_fazer_negocio = soup.find("span", class_="go4263471347").text.strip()
-    indice_de_solucao = soup.find("span", class_="go4263471347").text.strip()
-    nota_do_consumidor = soup.find("span", class_="go1306724026").text.strip()
+tree = html.fromstring(html_content)
 
-    # Extrair o texto do elemento encontrado
-    texto_reclamacoes = reclamacoes_respondidas.text.strip()
+# Extraindo os dados
+WebDriverWait(navegador, 10).until(
+        EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div[1]/div[1]/section/section/div[2]/div[2]/div[1]/div[2]/div[2]')))
 
-    # Adicionar os dados ao DataFrame
-    dados.append([texto_reclamacoes, voltariam_a_fazer_negocio, indice_de_solucao, nota_do_consumidor])
-except:
-    print("Erro")
+elements = tree.xpath('//span[@class="go2549335548"]')  # Encontra todos os spans com essa classe
+
+reclamacoes_respondidas = elements[1].text_content().strip()  # Segundo elemento
+voltariam_a_fazer_negocio = elements[4].text_content().strip()  # Quinto elemento
+indice_de_solucao = elements[5].text_content().strip()  # Sexto elemento
+
+nota_do_consumidor = soup.find("span", class_="go1306724026").text.strip()
+
+# Adicionar os dados ao DataFrame
+dados.append([reclamacoes_respondidas, voltariam_a_fazer_negocio, indice_de_solucao, nota_do_consumidor])
+
 
 
 # Criar o DataFrame e salvar em um arquivo Excel
-df = pd.DataFrame(dados, columns=["Nota", "Reclamações Respondidas", "Voltariam a Fazer Negócio", "Índice de Solução", "Nota do Consumidor"])
+df = pd.DataFrame(dados, columns=["Reclamações Respondidas", "Voltariam a Fazer Negócio", "Índice de Solução", "Nota do Consumidor"])
 df.to_excel("resultados.xlsx", index=False)
 
-time.sleep(5)
 """
 # Encontrar os elementos das listas (ajuste os seletores CSS conforme a estrutura da página)
 melhores_empresas = driver.find_elements_by_css_selector("seletor_css_para_as_melhores_empresas")
