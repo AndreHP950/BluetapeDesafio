8f8536e4b1887abc354d16180247121a49a0db03
ANDRE_PARREIRAS <parreirasandre950@gmail.com>
2024-08-26 16:31:29 -0300

    abrir primeira empresa

--- bluetape/main.py
+++ bluetape/main.py
@@ -4,6 +4,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 servico = Service(ChromeDriverManager().install())
 
@@ -13,11 +16,44 @@ navegador = webdriver.Chrome(service=servico)
 navegador.get("https://www.reclameaqui.com.br/")
 
 # Encontrar o botão "Moda" (ajuste o seletor CSS conforme a estrutura da página)
-botao_moda = navegador.find_element(By.CSS_SELECTOR, "button.botao-moda")  
+botao_moda = navegador.find_element(By.XPATH, "/html/body/section[2]/div/astro-island/div/nav/div[2]/button[4]")
 
 # Clicar no botão
 botao_moda.click()
 
+dados = []
+
+try:
+    botao_melhor1 = WebDriverWait(navegador, 10).until(
+        EC.presence_of_element_located((By.XPATH, "/html/body/section[2]/div/astro-island/div/div[3]/div/div[1]/a[1]/span"))
+    )
+    botao_melhor1.click()
+except:
+    print("Botão 'Moda' não encontrado")
+
+try:
+
+    html_content = navegador.page_source
+    soup = BeautifulSoup(html_content, 'html.parser')
+    reclamacoes_respondidas = WebDriverWait(navegador, 10).until(soup.find("span", class_="go2549335548"))
+    voltariam_a_fazer_negocio = soup.find("span", class_="go4263471347").text.strip()
+    indice_de_solucao = soup.find("span", class_="go4263471347").text.strip()
+    nota_do_consumidor = soup.find("span", class_="go1306724026").text.strip()
+
+    # Extrair o texto do elemento encontrado
+    texto_reclamacoes = reclamacoes_respondidas.text.strip()
+
+    # Adicionar os dados ao DataFrame
+    dados.append([texto_reclamacoes, voltariam_a_fazer_negocio, indice_de_solucao, nota_do_consumidor])
+except:
+    print("Erro")
+
+
+# Criar o DataFrame e salvar em um arquivo Excel
+df = pd.DataFrame(dados, columns=["Nota", "Reclamações Respondidas", "Voltariam a Fazer Negócio", "Índice de Solução", "Nota do Consumidor"])
+df.to_excel("resultados.xlsx", index=False)
+
+time.sleep(5)
 """
 # Encontrar os elementos das listas (ajuste os seletores CSS conforme a estrutura da página)
 melhores_empresas = driver.find_elements_by_css_selector("seletor_css_para_as_melhores_empresas")
